8c0f50816216ec80859e737e043a841e715b0248
Maija Mahlamäki <mahlamai@dhcp-asv-77.helsinki.fi>
2016-09-15 16:10:14 +0300

    alkeellinen versio tekstikäyttöliittymällä toteutettu

---
+++ Muistipeli/src/test/java/muistipeli/kayttoliittyma/KayttoliittymaTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muistipeli.kayttoliittyma;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mahlamai
+ */
+public class KayttoliittymaTest {
+    
+    public KayttoliittymaTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
